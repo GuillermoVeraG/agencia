5bdee250c236a6df1e9556ae274c3a4b5a428417
Guillermo Vera Ghanain <guilleverag@gmail.com>
2024-04-05 16:48:57 -0500

    fix blogs projects & prices margins and data

---
+++ src/content/projects/config.ts
@@ -0,0 +1,21 @@
+import { z, defineCollection } from "astro:content";
+
+// Define a `type` and `schema` for each collection
+const projectsCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    img: z.string(),
+    title: z.string(),
+    desc: z.string(),
+    tags: z.array(z.string()),
+    slug: z.string(),
+    client: z.string(),
+    pubDate: z.date(),
+    gallery: z.array(z.string()),
+  }),
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  projects: projectsCollection,
+};
